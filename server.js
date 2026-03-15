5e27dd35124cec4fd45b9e1f0ebc5f2a6d8d5ab7
User <user@example.com>
2026-03-15 18:16:19 +0800

    "fix_formidable_import"

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
-const formidable = require('formidable');
+const { formidable } = require('formidable');
 const CryptoJS = require('crypto-js');
 
 const PORT = process.env.PORT || 8080;
